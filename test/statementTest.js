c35870447a5e640a3fc5793aa9f5da4f3ef69c3c
hardy <hard.t.z.wei@oocl.com>
2020-08-27 14:44:24 +0800

    [exercise][hardy] test: add case2 for statement.

--- test/statementTest.js
+++ test/statementTest.js
@@ -1,79 +1,76 @@
 const test = require('ava');
-const {statement} = require('../src/statement');
+const {
+  statement
+} = require('../src/statement');
 
-test('Sample test', t => {
-  t.true(true);
-  t.is(1, 1);
-  t.deepEqual({a: 1}, {a: 1});
-});
 
 test('Case1 BigCo Buy Tickets', t => {
   //given
   const invoice = {
-      'customer': 'BigCo',
-      'performances': [
-          {
-              'playID': 'hamlet',
-              'audience': 55,
-          },
-          {
-              'playID': 'as-like',
-              'audience': 35,
-          },
-          {
-              'playID': 'othello',
-              'audience': 40,
-          },
-      ],
+    'customer': 'BigCo',
+    'performances': [{
+        'playID': 'hamlet',
+        'audience': 55,
+      },
+      {
+        'playID': 'as-like',
+        'audience': 35,
+      },
+      {
+        'playID': 'othello',
+        'audience': 40,
+      },
+    ],
+  };
+  const plays = {
+    'hamlet': {
+      'name': 'Hamlet',
+      'type': 'tragedy',
+    },
+    'as-like': {
+      'name': 'As You Like It',
+      'type': 'comedy',
+    },
+    'othello': {
+      'name': 'Othello',
+      'type': 'tragedy',
+    },
   };
   //when
   const result = statement(invoice, plays);
-  const expectResult = 'Statement for BigCo\n'
-      + ` Hamlet: $650.00 (55 seats)\n`
-      + ` As You Like It: $580.00 (35 seats)\n`
-      + ` Othello: $500.00 (40 seats)\n`
-      + `Amount owed is $1,730.00\n`
-      + `You earned 47 credits \n`;
+  const expectResult = 'Statement for BigCo\n' +
+    ` Hamlet: $650.00 (55 seats)\n` +
+    ` As You Like It: $580.00 (35 seats)\n` +
+    ` Othello: $500.00 (40 seats)\n` +
+    `Amount owed is $1,730.00\n` +
+    `You earned 47 credits \n`;
   //then
   t.is(result, expectResult);
 
 });
 
-test('Case2 ', t=> {
- //given
- 
-});
-
-const invoice = {
-  'customer': 'BigCo',
-  'performances': [
-    {
-      'playID': 'hamlet',
-      'audience': 55,
+test('Case2 Customer BigCo has one unknown performance.', t => {
+  //given
+  const plays = {
+    'othello': {
+      'name': 'Othello',
+      'type': 'tragedy1',
     },
-    {
-      'playID': 'as-like',
-      'audience': 35,
-    },
-    {
+  };
+  const invoice = {
+    'customer': 'BigCo',
+    'performances': [{
       'playID': 'othello',
       'audience': 40,
-    },
-  ],
-};
+    }, ],
+  };
+  //when
+  //then
+  try {
+    statement(invoice, plays);
+    t.fail();
+  } catch (e) {
+    t.is(e.message, 'unknown type: tragedy1');
+  }
+});
 
-
-const plays = {
-  'hamlet': {
-    'name': 'Hamlet',
-    'type': 'tragedy',
-  },
-  'as-like': {
-    'name': 'As You Like It',
-    'type': 'comedy',
-  },
-  'othello': {
-    'name': 'Othello',
-    'type': 'tragedy',
-  },
-};
