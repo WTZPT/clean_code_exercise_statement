e6c1f5b74b8246cc8a5407643973ab550b338e4f
hardy <hard.t.z.wei@oocl.com>
2020-08-27 15:53:06 +0800

    [exercise][hardy] refactor : extract calculateAmountByPlayTypeAndAudience memthod

--- src/statement.js
+++ src/statement.js
@@ -1,30 +1,41 @@
+const {
+  format
+} = require('./format')
 
-const { format } = require('./format')
+function calculateAmountByPlayTypeAndAudience(playType, audience) {
+  let thisAmount = 0;
+  switch (playType) {
+    case 'tragedy':
+      thisAmount = 40000;
+      if (audience > 30) {
+        thisAmount += 1000 * (audience - 30);
+      }
+      break;
+    case 'comedy':
+      thisAmount = 30000;
+      if (audience > 20) {
+        thisAmount += 10000 + 500 * (audience - 20);
+      }
+      thisAmount += 300 * audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${playType}`);
+  }
+  return {
+    thisAmount
+  }
+}
 
-function calculateAmount(performances,plays) {
+function calculateAmount(performances, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let str = '';
   for (let perf of performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
-    switch (play.type) {
-      case 'tragedy':
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy':
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
+
+    let {
+      thisAmount
+    } = calculateAmountByPlayTypeAndAudience(play.type, perf.audience)
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
@@ -33,19 +44,27 @@ function calculateAmount(performances,plays) {
     str += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
   }
-  return {str,totalAmount,volumeCredits}
+  return {
+    str,
+    totalAmount,
+    volumeCredits
+  }
 }
 
-function statement (invoice, plays) {
+function statement(invoice, plays) {
 
   let result = `Statement for ${invoice.customer}\n`;
-  
-  let {str,totalAmount,volumeCredits} = calculateAmount(invoice.performances,plays)
+
+  let {
+    str,
+    totalAmount,
+    volumeCredits
+  } = calculateAmount(invoice.performances, plays)
   result += str;
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits \n`;
   return result;
-} 
+}
 
 module.exports = {
   statement,
