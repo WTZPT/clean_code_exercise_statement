bc0db3c1e8d4304e55522a2f18724ce19d353e26
hardy <hard.t.z.wei@oocl.com>
2020-08-27 14:53:37 +0800

    [exercise][hardy] test: add case3 for statement.

--- test/statementTest.js
+++ test/statementTest.js
@@ -74,3 +74,47 @@ test('Case2 Customer BigCo has one unknown performance.', t => {
   }
 });
 
+test('Case3 BigCo Buy Tickets but hamlet audience less ', t => {
+  //given
+  const invoice = {
+    'customer': 'BigCo',
+    'performances': [{
+        'playID': 'hamlet',
+        'audience': 30,
+      },
+      {
+        'playID': 'as-like',
+        'audience': 35,
+      },
+      {
+        'playID': 'othello',
+        'audience': 40,
+      },
+    ],
+  };
+  const plays = {
+    'hamlet': {
+      'name': 'Hamlet',
+      'type': 'tragedy',
+    },
+    'as-like': {
+      'name': 'As You Like It',
+      'type': 'comedy',
+    },
+    'othello': {
+      'name': 'Othello',
+      'type': 'tragedy',
+    },
+  };
+  //when
+  const result = statement(invoice, plays);
+  const expectResult = 'Statement for BigCo\n' +
+    ` Hamlet: $400.00 (30 seats)\n` +
+    ` As You Like It: $580.00 (35 seats)\n` +
+    ` Othello: $500.00 (40 seats)\n` +
+    `Amount owed is $1,480.00\n` +
+    `You earned 22 credits \n`;
+  //then
+  t.is(result, expectResult);
+
+});
